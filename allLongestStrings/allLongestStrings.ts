5816788917a52516a2fb65c8a2b242de66d4820b
Rob <rlkeyer@gmail.com>
2021-04-06 21:31:45 -0400

    allLongestStrings solution

--- allLongestStrings/allLongestStrings.ts
+++ allLongestStrings/allLongestStrings.ts
@@ -1,5 +1,9 @@
 export function allLongestStrings(inputArray: string[]): string[] {
-
+    let longest = 0
+    inputArray.forEach((str) => {
+        if (str.length > longest) longest = str.length
+    })
+    return inputArray.filter((str) => str.length === longest)
 }
 
-console.log(allLongestStrings(["aba", "aa", "ad", "vcd", "aba"]));
+// console.log(allLongestStrings(["aba", "aa", "ad", "vcd", "aba"]));
