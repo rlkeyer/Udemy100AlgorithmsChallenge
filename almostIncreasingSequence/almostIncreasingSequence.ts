a86bed7bd5a42f0257e81364655b3c04f6098b7b
Rob <rlkeyer@gmail.com>
2021-04-06 21:44:17 -0400

    almostIncreasing solution

--- almostIncreasingSequence/almostIncreasingSequence.ts
+++ almostIncreasingSequence/almostIncreasingSequence.ts
@@ -1,6 +1,16 @@
 export function almostIncreasingSequence(sequence: number[]): boolean {
-
+    // iterate over array and check if next element is 1 more than current
+    // if not, set mulligan to true and move on
+    // if mulligan is already true, return false
+    let mulliganCount = 0
+    for (let i = 0; i < sequence.length - 1; i++) {
+        if (sequence[i] + 1 !== sequence[i + 1]) {
+            if (mulliganCount > 1) return false
+            mulliganCount += 1
+        }
+    }
+    return true
 }
 
-// console.log(almostIncreasingSequence([1, 3, 2, 1])) 
-// console.log(almostIncreasingSequence([1, 3, 2])) 
+console.log(almostIncreasingSequence([1, 3, 2, 1])) 
+console.log(almostIncreasingSequence([1, 3, 2])) 
