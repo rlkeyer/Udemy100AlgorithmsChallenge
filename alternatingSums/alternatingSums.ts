f99c84a48e70b614ba8c9c8fb80beb1390889cf9
Rob <rlkeyer@gmail.com>
2021-04-06 22:22:07 -0400

    alternatingSums solution

--- alternatingSums/alternatingSums.ts
+++ alternatingSums/alternatingSums.ts
@@ -1,5 +1,14 @@
 export function alternatingSums(a: number[]): number[] {
-
+    let team1 = 0
+    let team2 = 0
+    a.forEach((num, index) => {
+        if (index % 2 === 0) {
+            team1 += num
+        } else {
+            team2 += num
+        }
+    })
+    return [team1, team2]
 }
 
-console.log(alternatingSums([50, 60, 60, 45, 70]))
+// console.log(alternatingSums([50, 60, 60, 45, 70]))
