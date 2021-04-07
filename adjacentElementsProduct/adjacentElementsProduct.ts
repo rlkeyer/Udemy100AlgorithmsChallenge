7e3a265e8083b6ae8a05fdd011f75788298c5162
Rob <rlkeyer@gmail.com>
2021-04-06 21:27:02 -0400

    adjacentElements solution

--- adjacentElementsProduct/adjacentElementsProduct.ts
+++ adjacentElementsProduct/adjacentElementsProduct.ts
@@ -1,5 +1,11 @@
 export function adjacentElementsProduct(inputArray: number[]): number {
-
+    let highest = 0
+    // loop over input array and see if element + nextElement is greater than highest
+    inputArray.forEach((num, index) => {
+        const product = num * inputArray[index + 1]
+        if (product > highest) highest = product
+    })
+    return highest
 }
 
-//console.log(adjacentElementsProduct([3, 6, -2, -5, 7, 3]));
+// console.log(adjacentElementsProduct([3, 6, -2, -5, 7, 3]));
