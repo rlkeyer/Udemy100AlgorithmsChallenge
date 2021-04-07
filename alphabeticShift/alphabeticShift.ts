ea03671447b501aac0260d3c3e4bef871d4983aa
Rob <rlkeyer@gmail.com>
2021-04-06 21:59:05 -0400

    alphabeticShift solution

--- alphabeticShift/alphabeticShift.ts
+++ alphabeticShift/alphabeticShift.ts
@@ -1,5 +1,9 @@
 export function alphabeticShift(inputString: string): string {
-
+    const arr = inputString.split("")
+    return arr.map((char) => {
+        if (char === 'z') return 'a'
+        return String.fromCharCode(char.charCodeAt(0) + 1)
+    }).join("")
 }
 
 console.log(alphabeticShift('crazy'));
