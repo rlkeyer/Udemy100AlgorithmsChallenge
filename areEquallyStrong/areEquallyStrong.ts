297120c4241435a8c56fdafa910228cb0bfa2ec7
Rob <rlkeyer@gmail.com>
2021-04-07 23:16:09 -0400

    areEquallyStrong solution

--- areEquallyStrong/areEquallyStrong.ts
+++ areEquallyStrong/areEquallyStrong.ts
@@ -1,5 +1,10 @@
 export function areEquallyStrong(yourLeft: number, yourRight: number, friendsLeft: number, friendsRight: number): boolean {
+    const yourMax = Math.max(yourLeft, yourRight)
+    const yourMin = Math.min(yourLeft, yourRight)
+    const friendsMax = Math.max(friendsLeft, friendsRight)
+    const friendsMin = Math.min(friendsLeft, friendsRight)
 
+    return (yourMax === friendsMax && yourMin === friendsMin)
 }
 
 // console.log(areEquallyStrong(10, 15, 15, 10))
