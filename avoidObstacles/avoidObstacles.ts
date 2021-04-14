32c2342c918429ba5b9953c2f687868edbc60aa9
Rob <rlkeyer@gmail.com>
2021-04-13 21:41:56 -0400

    avoidObstacles solution

--- avoidObstacles/avoidObstacles.ts
+++ avoidObstacles/avoidObstacles.ts
@@ -1,5 +1,9 @@
 export function avoidObstacles(inputArray: number[]): number {
-
+    let jump = 2
+    while (inputArray.some((num) => num % jump === 0)) {
+        jump += 1
+    }
+    return jump
 }
 
-// console.log(avoidObstacles([5, 3, 6, 7, 9]));
+console.log(avoidObstacles([5, 3, 6, 7, 9]));
