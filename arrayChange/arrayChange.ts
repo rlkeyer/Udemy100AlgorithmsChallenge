0872ed597871c4beaf21c112cdda31c3c921bbc9
Rob <rlkeyer@gmail.com>
2021-04-13 20:28:34 -0400

    arrayChange solution

--- arrayChange/arrayChange.ts
+++ arrayChange/arrayChange.ts
@@ -1,5 +1,15 @@
 export function arrayChange(inputArray: number[]): number {
+    let moves = 0
 
+    for (let i = 0; i < inputArray.length; i++) {
+        if (inputArray[i] >= inputArray[i + 1]) {
+            const diff = (inputArray[i] + 1) - inputArray[i + 1]
+            inputArray[i + 1] = inputArray[i] + 1
+            moves += diff
+        }
+    }
+
+    return moves
 }
 
-// console.log(arrayChange([1, 1, 1]));
+console.log(arrayChange([1, 1, 1]));
