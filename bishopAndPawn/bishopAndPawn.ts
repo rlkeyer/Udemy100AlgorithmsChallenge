fd10366c07ae5252d57290553d944dfd0870c287
Rob <rlkeyer@gmail.com>
2021-04-17 00:05:52 -0400

    boxBlur solution

--- bishopAndPawn/bishopAndPawn.ts
+++ bishopAndPawn/bishopAndPawn.ts
@@ -10,15 +10,11 @@ export function bishopAndPawn(bishop: string, pawn: string): boolean {
         'h': 8,
     }
 
-    const bishopSplit = bishop.split('')
+    const bishopX = letterNumber[bishop[0]]
+    const bishopY = parseInt(bishop[1], 10)
 
-    const bishopX = letterNumber[bishopSplit[0]]
-    const bishopY = parseInt(bishopSplit[1], 10)
-
-    const pawnSplit = pawn.split('')
-
-    const pawnX = letterNumber[pawnSplit[0]]
-    const pawnY = parseInt(pawnSplit[1], 10)
+    const pawnX = letterNumber[pawn[0]]
+    const pawnY = parseInt(pawn[1], 10)
 
     return (Math.abs(bishopX - pawnX) === Math.abs(bishopY - pawnY))
 }
--- boxBlur/boxBlur.ts
+++ boxBlur/boxBlur.ts
@@ -1,7 +1,21 @@
 export function boxBlur(image: number[][]): number[][] {
-
+    const finalArr = []
+    for (let i = 1; i < image.length - 1; i++) {
+        const arr = []
+        for (let j = 1; j < image[i].length - 1; j++) {
+            const topRow = image[i - 1][j - 1] + image[i - 1][j] + image[i - 1][j + 1]
+            const middleRow = image[i][j - 1] + image[i][j] + image[i][j + 1]
+            const bottomRow = image[i + 1][j - 1] + image[i + 1][j] + image[i + 1][j + 1]
+            const avg = Math.floor((topRow + middleRow + bottomRow) / 9)
+            arr.push(avg)
+        }
+        finalArr.push(arr)
+    }
+    return finalArr
 }
 
-// console.log(boxBlur([[1, 1, 1], 
-//     [1, 7, 1], 
-//     [1, 1, 1]]));
+console.log(boxBlur([
+    [1, 1, 1, 1, 1], 
+    [1, 7, 1, 5, 1], 
+    [1, 1, 1, 4, 5]
+]));
