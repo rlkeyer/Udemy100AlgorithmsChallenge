64c652303095097d42c9dd189a4deb3477e2448b
Rob <rlkeyer@gmail.com>
2021-04-17 00:28:08 -0400

    refactoring

--- caseInsensitivePalimdrome/caseInsensitivePalindrome.ts
+++ caseInsensitivePalimdrome/caseInsensitivePalindrome.ts
@@ -1,10 +1,7 @@
 export function isCaseInsensitivePalindrome(inputString: string): boolean {
     const lowerCased = inputString.toLowerCase()
-    const len = inputString.length
-    const firstHalf = lowerCased.slice(0, Math.floor(len / 2))
-    const secondHalf = lowerCased.slice(Math.ceil(len / 2), len)
-    const reversedSecondHalf = secondHalf.split('').reverse().join('')
-    return (firstHalf === reversedSecondHalf)
+    const reversed = lowerCased.split('').reverse().join('')
+    return (lowerCased === reversed)
 }
 
 console.log(isCaseInsensitivePalindrome('AaBaa'));
