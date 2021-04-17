4457ca6cb7ff89678b12523f81d1329252186718
Rob <rlkeyer@gmail.com>
2021-04-17 00:50:41 -0400

    checkPalindrome solution

--- checkPalindrome/checkPalindrome.ts
+++ checkPalindrome/checkPalindrome.ts
@@ -1,7 +1,9 @@
 export function checkPalindrome(inputString: string): boolean {
-
+    const lowerCased = inputString.toLowerCase()
+    const reversed = lowerCased.split('').reverse().join('')
+    return lowerCased === reversed
 }
 
-// console.log(checkPalindrome('aabaa'));
-// console.log(checkPalindrome('abac'));
-// console.log(checkPalindrome('a'));
+console.log(checkPalindrome('aabaa'));
+console.log(checkPalindrome('abac'));
+console.log(checkPalindrome('a'));
