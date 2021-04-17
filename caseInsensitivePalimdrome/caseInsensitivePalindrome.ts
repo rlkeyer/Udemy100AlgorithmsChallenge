7a7bc4892aefa3563cfdcb45aa0594ca45c239e2
Rob <rlkeyer@gmail.com>
2021-04-17 00:23:49 -0400

    caseInsensitivePalindrome solution

--- caseInsensitivePalimdrome/caseInsensitivePalindrome.ts
+++ caseInsensitivePalimdrome/caseInsensitivePalindrome.ts
@@ -1,6 +1,11 @@
 export function isCaseInsensitivePalindrome(inputString: string): boolean {
-
+    const lowerCased = inputString.toLowerCase()
+    const len = inputString.length
+    const firstHalf = lowerCased.slice(0, Math.floor(len / 2))
+    const secondHalf = lowerCased.slice(Math.ceil(len / 2), len)
+    const reversedSecondHalf = secondHalf.split('').reverse().join('')
+    return (firstHalf === reversedSecondHalf)
 }
 
-// console.log(isCaseInsensitivePalindrome('AaBaa'));
-// console.log(isCaseInsensitivePalindrome('abac'));
+console.log(isCaseInsensitivePalindrome('AaBaa'));
+console.log(isCaseInsensitivePalindrome('abac'));
