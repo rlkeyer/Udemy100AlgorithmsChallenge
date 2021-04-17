c9063d55fb31b2822d514787020e636c93ac16fd
Rob <rlkeyer@gmail.com>
2021-04-17 00:47:57 -0400

    character parity solution

--- characterParity/characterParity.ts
+++ characterParity/characterParity.ts
@@ -1,7 +1,11 @@
 export function characterParity(symbol: string): string {
-
+    console.log(parseInt(symbol, 10))
+    const parsedSymbol = parseInt(symbol, 10)
+    if (isNaN(parsedSymbol)) return 'not a digit'
+    if (parsedSymbol % 2 === 0) return 'even'
+    return 'odd'
 }
 
-// console.log(characterParity('5'))
-// console.log(characterParity('8'))
-// console.log(characterParity('q'))
+console.log(characterParity('5'))
+console.log(characterParity('8'))
+console.log(characterParity('q'))
