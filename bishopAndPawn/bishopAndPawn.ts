df6645b433b3ec64e3e9b6e1c3a6e4418df4de66
Rob <rlkeyer@gmail.com>
2021-04-16 23:37:54 -0400

    bishopAndPawn solution

--- bishopAndPawn/bishopAndPawn.ts
+++ bishopAndPawn/bishopAndPawn.ts
@@ -1,5 +1,26 @@
 export function bishopAndPawn(bishop: string, pawn: string): boolean {
+    const letterNumber = {
+        'a': 1,
+        'b': 2,
+        'c': 3,
+        'd': 4,
+        'e': 5,
+        'f': 6,
+        'g': 7,
+        'h': 8,
+    }
 
+    const bishopSplit = bishop.split('')
+
+    const bishopX = letterNumber[bishopSplit[0]]
+    const bishopY = parseInt(bishopSplit[1], 10)
+
+    const pawnSplit = pawn.split('')
+
+    const pawnX = letterNumber[pawnSplit[0]]
+    const pawnY = parseInt(pawnSplit[1], 10)
+
+    return (Math.abs(bishopX - pawnX) === Math.abs(bishopY - pawnY))
 }
 
-// console.log(bishopAndPawn('a1', 'c3'));
+console.log(bishopAndPawn('a2', 'g8'));
