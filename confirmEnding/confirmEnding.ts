d9eba9acaf06cee63905f47479ddac9a202d71b6
Rob <rlkeyer@gmail.com>
2021-04-18 00:51:08 -0400

    confirmEnding solution

--- confirmEnding/confirmEnding.ts
+++ confirmEnding/confirmEnding.ts
@@ -1,6 +1,9 @@
 export function confirmEnding(str: string, target: string) {
+    const strLen = str.length
+    const targetLen = target.length
 
+    return str.substr(strLen - targetLen) === target
 }
 
-// console.log(confirmEnding("Abstraction", "action"));
-// console.log(confirmEnding("Open sesame", "pen"));
+console.log(confirmEnding("Abstraction", "action"));
+console.log(confirmEnding("Open sesame", "pen"));
